a9b143c833b0bbdc4acfa890bb4bdc1ee797002c
VAV <vav@voxteneo.com.mk>
2019-10-20 19:11:21 +0200

    inital commit

---
+++ DockCore/DockCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DockCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
